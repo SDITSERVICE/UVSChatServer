feb38431e86c6f2707ebd6f1c8ea09da689143dc
Roman <anomal3@yandex.ru>
2024-06-17 13:28:13 +0400

    Update server and client
    Адаптирован клиент для подключения к любому серверу.
    Изменён кодек с G711 на PCMA

--- SampleClient/Program.cs
+++ SampleClient/Program.cs
@@ -1,7 +1,6 @@
-﻿using System.Diagnostics;
-using LumiSoft.Net.Codec;
-using LumiSoft.Net.UDP;
+﻿using LumiSoft.Net.Media.Codec.Audio;
 using Microsoft.AspNetCore.SignalR.Client;
+using System.Diagnostics;
 using UVSChatMedia.Media.Wave;
 using UVSMedia.Media;
 
@@ -10,7 +9,6 @@ namespace SampleClient
 {
     internal class Program
     {
-        private static int m_Codec = 0;
         private static HubConnection m_connection;
         private static WaveOut m_WaveOut;
         private static WaveIn m_WaveIn;
@@ -19,45 +17,99 @@ namespace SampleClient
 
         static async Task Main(string[] args)
         {
+            Console.WriteLine("Enter ip address. Default 127.0.0.1");
+            var inputIp = Console.ReadLine();
+            var ip = string.IsNullOrEmpty(inputIp) ? "127.0.0.1" : inputIp;
+
+
+            Console.WriteLine("Enter port. Default 5000");
+            var inputPortp = Console.ReadLine();
+            var port = string.IsNullOrEmpty(inputPortp) ? "5000" : inputPortp;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Connecting");
+
+
+            await Task.Delay(500);
+            Console.Write(".");
+            await Task.Delay(500);
+            Console.Write(".");
+            await Task.Delay(500);
+            Console.Write(".");
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+
             m_connection = new HubConnectionBuilder()
-                .WithUrl("http://localhost:5000/chat") // replace your URL
+                .WithUrl($"http://{ip}:{port}/chat") // replace your URL
                 .Build();
 
             m_connection.On<byte[]>(nameof(ClientCmd.ReceiveAudio), audioData =>
             {
-                
+
                 Debug.WriteLine($"Recive fragment {Utils.ByteArrayToHexString(audioData)} frLeg {audioData.Length}");
                 m_PacketReceived(audioData);
-               
+
             });
 
-            await m_connection.StartAsync();
+
+            await Task.Delay(500);
+            Console.Write(".");
+            await Task.Delay(500);
+            Console.WriteLine(".");
+
+            try
+            {
+                await m_connection.StartAsync();
+            }
+
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
 
             #region Show devices
 
-            //TODO : Create choise
-            // WaveIn.Devices
-            //     .ToList()
-            //     .ForEach(device =>
-            //         Console.WriteLine($"In: Name {device.Name}")
-            //     );
+            Console.WriteLine();
+            Console.WriteLine($"Find devices");
+            Console.WriteLine();
 
-            // WaveOut.Devices
-            //     .ToList()
-            //     .ForEach(device =>
-            //         Console.WriteLine($"Out: Name {device.Name}")
-            //     );
-            
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            //TODO : Create choise
+            WaveIn.Devices
+                .ToList()
+                .ForEach(device =>
+                    Console.WriteLine($"Mic: name {device.Name}")
+                );
+
+            WaveOut.Devices
+                .ToList()
+                .ForEach(device =>
+                    Console.WriteLine($"Speak's: name {device.Name}")
+                );
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
 
             #endregion
 
-            Console.WriteLine("Connection started. Wait other user.\r\nPress any key to exit...");
+            if (m_connection.State == HubConnectionState.Connected)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Connection successful.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Нажми любую клавишу чтобы выйти...");
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Connection to server refused");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
             await Task.Delay(2000);
 
             //m_pRecordStream = File.Create($"D:\\test{new Random().Next(1, 10000)}.wav");
 
-            m_WaveOut = new WaveOut(WaveOut.Devices[0], 8000, 16, 1);
-            m_WaveIn = new WaveIn(WaveIn.Devices[0], 8000, 16, 1, 256);
+            m_WaveOut = new WaveOut(WaveOut.Devices[0], 16000, 16, 1);
+            m_WaveIn = new WaveIn(WaveIn.Devices[0], 16000, 16, 1, 256);
             m_WaveIn.BufferFull += new BufferFullHandler(m_pWaveIn_BufferFull);
             m_WaveIn.Start();
 
@@ -67,34 +119,27 @@ namespace SampleClient
 
         private static void m_pWaveIn_BufferFull(byte[] buffer)
         {
+            PCMA pcma = new PCMA();
             // Compress data. 
             byte[] encodedData = null;
-            if (m_Codec == 0)
-            {
-                encodedData = G711.Encode_aLaw(buffer, 0, buffer.Length);
-            }
-            else if (m_Codec == 1)
-            {
-                encodedData = G711.Encode_uLaw(buffer, 0, buffer.Length);
-            }
+
+            //encodedData = G711.Encode_aLaw(buffer, 0, buffer.Length);
+            encodedData = pcma.Encode(buffer, 0, buffer.Length);
+
 
             m_connection.SendAsync(nameof(ServerCmd.SendAudio), encodedData);
-            
+
         }
 
-        //TODO : Change codec as PCMA
+        //PATCH : Change codec as PCMA
         private static void m_PacketReceived(byte[] e)
         {
+            PCMA pcma = new PCMA();
             // Decompress data.
             byte[] decodedData = null;
-            if (m_Codec == 0)
-            {
-                decodedData = G711.Decode_aLaw(e, 0, e.Length);
-            }
-            else if (m_Codec == 1)
-            {
-                decodedData = G711.Decode_uLaw(e, 0, e.Length);
-            }
+
+            //decodedData = G711.Decode_aLaw(e, 0, e.Length);
+            decodedData = pcma.Decode(e, 0, e.Length);
 
             // We just play received packet.
             m_WaveOut.Play(decodedData, 0, decodedData.Length);
--- UVSChatServer/Hubs/ChatHub.cs
+++ UVSChatServer/Hubs/ChatHub.cs
@@ -8,11 +8,14 @@ namespace UVSChatServer.Hubs
 {
     public class ChatHub : Hub
     {
+        #region TODO : Update
+
         public static List<string> _groups { get; set; } = new List<string>();
         public static List<string> AllMessages { get; set; } = new List<string>();
 
         public static Action<string>? ActionMessage;
 
+        #endregion
 
         public override Task OnConnectedAsync()
         {
@@ -26,54 +29,25 @@ namespace UVSChatServer.Hubs
         public async Task SendAudio(byte[] audioData)
         {
             Console.WriteLine($"Data={Utils.ByteArrayToHexString(audioData)}\r\nLength={audioData.Length}");
-            //await Clients.Caller.SendAsync(nameof(ClientCmd.ReceiveAudio), audioData); //Отправить всем кто подписан на событие ReciveAudio
-            await Clients.Others.SendAsync(nameof(ClientCmd.ReceiveAudio), audioData); //Отправить всем кто подписан на событие ReciveAudio
+            await Clients.Others.SendAsync(nameof(ClientCmd.ReceiveAudio), audioData); //Send to everyone who is subscribed to the Receive Audio event
         }
 
         [HubMethodName(nameof(ServerCmd.SendAudioUnity))]
         public async Task SendAudioUnity(float[] audioData)
         {
-            //// Открываем файл для записи в бинарном режиме
+            #region Test write to RAW file
+
+            //// Opening a file for writing in binary mode
             //using (FileStream fs = new FileStream("D:\\test.raw", FileMode.Append, FileAccess.Write))
             //{
-            //    // Записываем массив байт в файл
+            //    // write to file
             //    fs.Write(ConvertFloatToByte(audioData), 0, ConvertFloatToByte(audioData).Length);
             //}
 
-            //Console.WriteLine($"Data={string.Join(" ",audioData)}\r\nLength={audioData.Length}");
-            await Clients.Caller.SendAsync(nameof(ClientCmd.ReceiveAudio), audioData); //Отправить всем кто подписан на событие ReciveAudio
-            //await Clients.Others.SendAsync(nameof(ClientCmd.ReceiveAudio), audioData); //Отправить всем кто подписан на событие ReciveAudio
-        }
+            #endregion
 
-        private byte[] ConvertFloatToByte(float[] array)
-        {
-            byte[] byteArr = new byte[array.Length * 4];
-            for (int i = 0; i < array.Length; i++)
-            {
-                var bytes = BitConverter.GetBytes(array[i] * 0x80000000);
-                Array.Copy(bytes, 0, byteArr, i * 4, bytes.Length);
-                if (BitConverter.IsLittleEndian)
-                    Array.Reverse(byteArr, i * 4, 4);
-            }
-            return byteArr;
-        }
-
-        public ChannelReader<byte[]> UploadStream(ChannelReader<byte[]> stream)
-        {
-            _ = Task.Run(async () =>
-            {
-                while (await stream.WaitToReadAsync())
-                {
-                    while (stream.TryRead(out var item))
-                    {
-                        // Отправить полученные данные всем клиентам
-                        await Clients.Others.SendAsync("ReceiveAudioPacket", item);
-                    }
-                }
-            });
-
-            var channel = Channel.CreateUnbounded<byte[]>();
-            return channel.Reader;
+            await Clients.Caller.SendAsync(nameof(ClientCmd.ReceiveAudio), audioData);
+           
         }
 
     }
